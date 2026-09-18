e022669de6d995624de6a7050eaf96cee10913f5
chlwnstkd <jun1234755@gmail.com>
2026-09-18 12:07:43 +0900

    0017

--- src/main/java/com/back/boundedContext/post/domain/PostMember.java
+++ src/main/java/com/back/boundedContext/post/domain/PostMember.java
@@ -1,7 +1,7 @@
 package com.back.boundedContext.post.domain;
 
 
-import com.back.global.jpa.entity.BaseIdAndTime;
+import com.back.shared.member.domain.ReplicaMember;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
@@ -10,12 +10,10 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "POST_MEMBER")
-@NoArgsConstructor
 @Getter
-public class PostMember extends BaseIdAndTime {
-    @Column(unique = true)
-    private String username;
-    private String password;
-    private String nickname;
-    private int activityScore;
+@NoArgsConstructor
+public class PostMember extends ReplicaMember {
+    public PostMember(String username, String password, String nickname) {
+        super(username, password, nickname);
+    }
 }
