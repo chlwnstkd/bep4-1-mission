efb234d2442d4240393b7a66b5bb5524cd180798
chlwnstkd <jun1234755@gmail.com>
2026-09-18 10:46:59 +0900

    0014

---
+++ src/main/java/back/boundedContext/member/app/MemberFacade.java
@@ -0,0 +1,43 @@
+package com.back.boundedContext.member.app;
+
+import com.back.boundedContext.member.domain.Member;
+import com.back.boundedContext.member.out.MemberRepository;
+import com.back.global.exception.DomainException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.back.global.rsData.RsData;
+import java.util.Optional;
+import com.back.boundedContext.member.domain.MemberPolicy;
+
+@Service
+@RequiredArgsConstructor
+public class MemberFacade {
+    private final MemberRepository memberRepository;
+    private final MemberJoinUseCase memberJoinUseCase;
+    private final MemberPolicy memberPolicy;
+
+    public long count() {
+        return memberRepository.count();
+    }
+
+    @Transactional
+    public RsData<Member> join(String username, String password, String nickname) {
+        return memberJoinUseCase.join(username, password, nickname);
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Member> findByUsername(String username) {
+        return memberRepository.findByUsername(username);
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Member> findById(int id) {
+        return memberRepository.findById(id);
+    }
+
+    public String getRandomSecureTip() {
+        return "비밀번호의 유효기간은 %d일 입니다."
+                .formatted(memberPolicy.getNeedToChangePasswordDays());
+    }
+}
